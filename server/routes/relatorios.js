467e8fc2baf5fc1dc58deda7f6a0088e760be52b
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-07-19 08:21:18 -0300

    Conexão do Banco de dados em arquivo separado

--- server/routes/relatorios.js
+++ server/routes/relatorios.js
@@ -1,16 +1,6 @@
 const express = require('express')
 const relatorios = express.Router()
-
-const { Client } = require('pg');
-
-const cliente = new Client({
-  connectionString: "postgres://vbqbxvwduupnpi:7126070db19f0b620bc80a0eb041e8067a9eaf9b568937002331f2eb3f51ffc8@ec2-23-23-182-238.compute-1.amazonaws.com:5432/dcte0q2jbf9d90",
-  ssl: {
-    rejectUnauthorized: false
-  }
-})
-
-cliente.connect()
+var cliente = require('../database/connection.js')
 
 // Mostrando todas as atualizações
 relatorios.get('/relatorios/projetos', (req, res) => { 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 
-const { Client } = require('pg');
-
-const cliente = new Client({
-  connectionString: "postgres://vbqbxvwduupnpi:7126070db19f0b620bc80a0eb041e8067a9eaf9b568937002331f2eb3f51ffc8@ec2-23-23-182-238.compute-1.amazonaws.com:5432/dcte0q2jbf9d90",
-  ssl: {
-    rejectUnauthorized: false
-  }
-})
-
+var cliente = require('../database/connection.js')
 cliente.connect()
 
 const pessoas = require('./pessoas')
