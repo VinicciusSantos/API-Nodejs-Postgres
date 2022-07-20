3b7b186d7fbd8fe9cd8db24801d0c6277fc79af9
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-07-20 11:51:19 -0300

    Relatorios de quantidade de tarefas concluidas por uma pessoa em um determinado mês

--- server/routes/relatorios.js
+++ server/routes/relatorios.js
@@ -2,7 +2,7 @@ const express = require('express')
 const relatorios = express.Router()
 var cliente = require('../database/connection.js')
 
-// Mostrando todas as atualizações
+
 relatorios.get('/relatorios/projetos', (req, res) => { 
     cliente
         .query(`SELECT
@@ -12,6 +12,20 @@ relatorios.get('/relatorios/projetos', (req, res) => {
                 WHERE pr_data_finalizacao IS NOT NULL
                 GROUP BY EXTRACT(MONTH from pr_data_finalizacao), EXTRACT(YEAR FROM pr_data_finalizacao)
                 ORDER BY EXTRACT(MONTH from pr_data_finalizacao), EXTRACT(YEAR FROM pr_data_finalizacao)`)
+        .then(results => {
+        return res.json(results.rows)
+    })
+})
+
+relatorios.get('/relatorios/pessoas/:id', (req, res) => { 
+    const id = req.params.id
+
+    cliente
+        .query(`SELECT EXTRACT(MONTH FROM tr.tr_data_finalizacao) AS mes, EXTRACT(YEAR FROM tr.tr_data_finalizacao) AS ano, COUNT(*) FROM tarefas AS tr
+                INNER JOIN pessoas_associam_tarefas AS pat ON pat.fk_tarefa = tr.tr_id
+                INNER JOIN pessoas AS pe ON pe.pe_id = pat.fk_pessoa
+                WHERE pe.pe_id = $1 AND tr.tr_status = 'Concluido'
+                GROUP BY EXTRACT(MONTH FROM tr.tr_data_finalizacao), EXTRACT(YEAR FROM tr.tr_data_finalizacao)`, [id])
         .then(results => {
         return res.json(results.rows)
     })
