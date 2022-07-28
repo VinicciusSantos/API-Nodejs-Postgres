81390de89999593e15bfe1558e4942e8237d6d8c
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-07-28 11:22:29 -0300

    Recebendo nome ao inves de iDs

--- server/routes/equipes.js
+++ server/routes/equipes.js
@@ -38,13 +38,17 @@ equipes.post('/equipes', async (req, res) => {
     // cadastrando a equipe
     cliente.query('INSERT INTO equipes (eq_nome) values ($1)', [body.eq_nome])
 
+    const pessoas = await cliente.query(`select pe_nome, pe_id from pessoas`)
+
     // Pegando o id da equipe que acabou de ser cadastrada
     const id = await cliente.query('select max(eq_id) from equipes')
 
+
     // Colocando as pessoas na equipe
-    body.pessoas.forEach(p => {
+    body.pessoas.forEach(async p => {
+        const idPessoa = await cliente.query(`select pe_id from pessoas where pr_nome = $1`, [p])
         cliente.query(`INSERT INTO pessoas_pertencem_equipes (fk_pessoa, fk_equipe)
-                   VALUES ($1, $2)`, [p, id.rows[0].max])
+                VALUES ($1, $2)`, [idPessoa.rows[0].pe_id, id.rows[0].max])
     });
 
     return res.json("Inserido com sucesso!")
