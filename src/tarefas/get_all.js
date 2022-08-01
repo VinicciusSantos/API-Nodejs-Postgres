c9bc002282e2a602e88e85c47abe3e2bb2623a96
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-08-01 08:53:30 -0300

    Mostar o projeto que  a tarefa está em /tarefas

--- src/tarefas/get_all.js
+++ src/tarefas/get_all.js
@@ -5,7 +5,10 @@ var cliente = require('../../cmd/database/connection.js')
 // Mostrando todas as tarefas
 tarefas.get('/tarefas', (req, res) => { 
     cliente
-        .query(`SELECT * FROM tarefas ORDER BY tr_id`)
+        .query(`SELECT tr.*, pr.pr_nome from tarefas AS tr
+                INNER JOIN projetos_possuem_tarefas AS ppt ON ppt.fk_tarefa = tr.tr_id
+                INNER JOIN projetos AS pr ON pr.pr_id = ppt.fk_projeto
+                ORDER BY tr.tr_nome`)
         .then(results => {
         return res.json(results.rows)
     })
