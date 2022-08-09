23945a2fc398b041f075eaad4266e94992ede216
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-08-09 09:56:23 -0300

    Associar pessoas e projeto quando  uma tarefa é criada

--- src/pessoas/add_pessoa.js
+++ src/pessoas/add_pessoa.js
@@ -6,11 +6,8 @@ var cliente = require("../../cmd/database/connection.js");
 pessoas.post("/pessoas", (req, res) => {
     const body = req.body;
     cliente
-        .query(
-            `INSERT INTO pessoas (pe_nome, pe_cargo, pe_salario, pe_data_nasc, pe_status)
-                VALUES ($1, $2, $3, $4, $5)`, [ body.pe_nome, body.pe_cargo, body.pe_salario, body.pe_data_nasc, "Não Iniciado",
-            ]
-        )
+        .query(`INSERT INTO pessoas (pe_nome, pe_cargo, pe_salario, pe_data_nasc, pe_status)
+                VALUES ($1, $2, $3, $4, $5)`, [ body.pe_nome, body.pe_cargo, body.pe_salario, body.pe_data_nasc, "Não Iniciado",])
         .then((results) => {
             return res.json("Inserido com sucesso!");
         })
--- src/tarefas/add_tarefa.js
+++ src/tarefas/add_tarefa.js
@@ -6,10 +6,38 @@ var cliente = require('../../cmd/database/connection.js')
 tarefas.post('/tarefas', async (req, res) => { 
     const body = req.body
 
-    const results = await cliente.query('INSERT INTO tarefas (tr_nome, tr_descricao, tr_data_criacao, tr_status, tr_prioridade) values ($1, $2, $3, $4, $5)', [body.tr_nome, body.tr_descricao, body.tr_data_criacao, 'Inicio', body.tr_prioridade])
-    .catch(e => {        
-        return res.status(400).json(e)
-    })
+    // recebendo a quantidadde de tarefas que tem o mesmo nome do que foi passado no body pra fazer as validações
+    const count = await cliente.query('SELECT count(*) from tarefas where tr_nome ilike $1', [body.tr_nome])
+    
+    if (count.rows[0].count != 0) {
+        return res.status(404).json("Uma tarefa com esse nome já foi inserida")
+    }
+
+    // Inserindo a tarefa
+    const results = await cliente.query(`INSERT INTO tarefas (tr_nome, tr_descricao, tr_data_criacao, tr_status, tr_prioridade)
+                                    VALUES ($1, $2, current_date, 'Em Andamento', $3)`, [body.tr_nome, body.tr_descricao, body.tr_prioridade])
+    
+    // Pegando o id da tarefa que acabou de ser cadastrada
+    const id = await cliente.query('select max(tr_id) from tarefas')
+    
+    if (body.pr_id) {
+        // Associando a tarefa com o projeto
+        const projetoTarefa = await cliente.query(`INSERT INTO projetos_possuem_tarefas (fk_projeto, fk_tarefa)
+                                                    VALUES ($1, $2)`, [body.pr_id, id.rows[0].max])
+    }
+    
+    
+    if (body.pessoas) {   
+        // Associando as pessoas com as tarefas
+        body.pessoas.forEach(async p => {
+            const idPessoa = await cliente.query(`select pe_id from pessoas where pe_nome = $1`, [p])
+            
+            cliente.query(`INSERT INTO pessoas_associam_tarefas (fk_pessoa, fk_tarefa)
+            VALUES ($1, $2)`, [idPessoa.rows[0].pe_id, id.rows[0].max])
+        })
+    }
+
+    return res.status(200).json(`Deu Certo`)
 })
 
 module.exports = tarefas
