8f414432a36bdfe89930099297d9b88b1224aa51
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-08-10 09:16:30 -0300

    Revert "Tratamento de posts sem nomes vazios"
    This reverts commit 210c1d7fb0f25aeeabef8a3247d9ffa73791d450.

--- src/pessoas/add_pessoa.js
+++ src/pessoas/add_pessoa.js
@@ -5,11 +5,6 @@ var cliente = require("../../cmd/database/connection.js");
 // Inserindo pessoas
 pessoas.post("/pessoas", (req, res) => {
     const body = req.body;
-
-    if (body.pe_nome === "" || !body.pe_nome) {
-        return res.status(400).json('Nome Inválido')
-    }
-
     cliente
         .query(`INSERT INTO pessoas (pe_nome, pe_cargo, pe_salario, pe_data_nasc, pe_status)
                 VALUES ($1, $2, $3, $4, $5)`, [ body.pe_nome, body.pe_cargo, body.pe_salario, body.pe_data_nasc, "Não Iniciado",])
