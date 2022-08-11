50d25ed250e04e14f3120452da69db1ab21e421c
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-08-11 15:20:08 -0300

    Update get_equipe.js

--- src/equipes/get_equipe.js
+++ src/equipes/get_equipe.js
@@ -3,17 +3,43 @@ const equipes = express.Router()
 var cliente = require('../../cmd/database/connection.js')
 
 // Mostrando equipes específicas pelo ID
-equipes.get('/equipes/:id', (req, res) => { 
+equipes.get('/equipes/:id', async (req, res) => { 
     const id = req.params.id
 
-    cliente
-        .query('SELECT * FROM equipes WHERE eq_id = $1', [id])
-        .then(results => {
-            return res.json(results.rows[0])
-        })
-        .catch(e => {         
-            return res.status(400).json(e)
-        })
+    // Recebendo todas as informações da equipe
+    const dados = cliente
+                    .query('SELECT * FROM equipes WHERE eq_id = $1', [id])
+                    .catch(e => {         
+                        return res.status(400).json(e)
+                    })
+    
+    const pessoas = cliente
+                        .query(`SELECT pe.* FROM equipes AS eq
+                                INNER JOIN pessoas_pertencem_equipes AS ppe ON ppe.fk_equipe = eq.eq_id
+                                INNER JOIN pessoas AS pe ON pe.pe_id = ppe.fk_pessoa
+                                WHERE eq.eq_id = $1`, id)
+                        .catch(e => {         
+                            return res.status(400).json(e)
+                        })
+
+    const results = dados.rows[0]
+    results.pessoas = pessoas.rows
+
+    const tarefas = cliente
+                        .query(`SELECT tr.*, eq.eq_id, pe.pe_id FROM equipes AS eq
+                                INNER JOIN pessoas_pertencem_equipes AS ppe ON ppe.fk_equipe = eq.eq_id
+                                INNER JOIN pessoas AS pe ON pe.pe_id = ppe.fk_pessoa
+                                INNER JOIN pessoas_associam_tarefas AS pat ON pat.fk_pessoa = pe.pe_id
+                                INNER JOIN tarefas AS tr ON tr.tr_id = pat.fk_tarefa
+                                WHERE eq.eq_id = $1`, id)
+                        .catch(e => {         
+                            return res.status(400).json(e)
+                        })
+    
+    pessoas.rows.forEach((p, index) => {
+        results.pessoas[index].tarefas = tarefas.filter(t => t.pe_id = p.pe_id)
+    })
+    
 })
 
 module.exports = equipes
