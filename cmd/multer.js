383e1714158311fa8d08304f69aff667803cae8b
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-08-15 09:11:42 -0300

    Tratamento de erros da função de get Equipe

--- cmd/multer.js
+++ cmd/multer.js
@@ -1,30 +1,11 @@
 const multer = require("multer");
-const path = require("path");
-const crypto = require("crypto")
-const multerS3 = require("multer-s3");
-const uuid = require("uuid").v4
-
-const MAX_SIZE_TWO_MEGABYTES = 2 * 1024 * 1024;
-
-// const storageTypes = {
-//     local: multer.diskStorage({
-//         destination: (req, file, cb) => {
-//           cb(null, "./uploads");
-//         },
-//         filename: (req, file, cb) => {
-//             const { originalname } = file
-//             cb(null, `${uuid()}-${originalname}`)
-//         },
-//     }),
-// };
-
 const storage = multer.memoryStorage()
 
 module.exports = {
   dest: "./uploads",
   storage: storage,
   limits: {
-    fileSize: MAX_SIZE_TWO_MEGABYTES,
+    fileSize: 2 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
     const allowedMimes = [
--- src/equipes/get_equipe.js
+++ src/equipes/get_equipe.js
@@ -7,14 +7,22 @@ equipes.get('/equipes/:id', async (req, res) => {
     const id = req.params.id
 
     // Recebendo todas as informações da equipe
-    const dados = await cliente
-                    .query('SELECT * FROM equipes WHERE eq_id = $1', [id])
+    try {
+        var dados = await cliente.query('SELECT * FROM equipes WHERE eq_id = $1', [id])
+    } catch (err) {
+        return res.status(400).send(err.stack)
+    }
     
+    if (dados.rowCount == 0) return res.status(404).json(`Nenhuma Equipe Encontrada com o ID: ${id}`)
+
     const pessoas = await cliente
                         .query(`SELECT pe.* FROM equipes AS eq
                                 INNER JOIN pessoas_pertencem_equipes AS ppe ON ppe.fk_equipe = eq.eq_id
                                 INNER JOIN pessoas AS pe ON pe.pe_id = ppe.fk_pessoa
                                 WHERE eq.eq_id = $1`, [id])
+                        .catch(e => {                             
+                            return res.status(400).json(e)
+                        })
 
     const results = dados.rows[0]
     results.pessoas = pessoas.rows
@@ -26,7 +34,10 @@ equipes.get('/equipes/:id', async (req, res) => {
                                 INNER JOIN pessoas_associam_tarefas AS pat ON pat.fk_pessoa = pe.pe_id
                                 INNER JOIN tarefas AS tr ON tr.tr_id = pat.fk_tarefa
                                 WHERE eq.eq_id = $1`, [id])
-    
+                        .catch(e => {                             
+                            return res.status(400).json(e)
+                        })
+
     pessoas.rows.forEach((p, index) => {
         results.pessoas[index].tarefas = tarefas.rows.filter(t => t.pe_id === p.pe_id)
     })
