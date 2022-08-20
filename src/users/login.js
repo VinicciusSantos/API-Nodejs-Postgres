f1401fbb867f901e6375ecaab6a855f643715768
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2022-08-20 16:30:27 -0300

    Registro e Login com JWT

---
+++ src/users/login.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const express = require('express')
+const user = express.Router()
+const cliente = require('../../cmd/database/connection.js')
+const bcrypt = require('bcrypt')
+
+//Inserindo Tarefas
+user.post('/login', async (req, res) => {
+    let { usuario, senha } = req.body
+
+    if (!usuario || !senha)
+        return res.status(400).json(`Valores Obrigatórios não recebidos`)
+
+    const user = await cliente
+                            .query(`SELECT * FROM users WHERE usuario = $1`, [usuario])
+                            .catch(e => {                                  
+                                return res.status(400).json(e)
+                            })
+    
+    if (user.rowCount == 0) {
+        return res.status(404).json({ message: "Usuário Não Encontrado" });
+    }
+
+    const validPassword = await bcrypt.compare(senha, user.rows[0].senha);
+    if (validPassword) {
+        const id = user.rows[0].id
+        const token = jwt.sign({ id }, process.env.SECRET, {
+            expiresIn: 900 // expires in 15min
+        });
+        return res.status(200).json({ auth: true, token: token });
+    } else {
+        res.status(401).json({ auth: false });
+    }
+})
+
+module.exports = user
--- src/users/register.js
+++ src/users/register.js
@@ -4,55 +4,31 @@ const cliente = require('../../cmd/database/connection.js')
 const bcrypt = require('bcrypt')
 
 //Inserindo Tarefas
-user.post('/registro', async (req, res) => {
-    let { nome, email, senha, confirmacao } = req.body
+user.post('/cadastro', async (req, res) => {
+    let { usuario, senha } = req.body
 
-    if (!nome || !email || !senha || !confirmacao)
+    if (!usuario || !senha)
         return res.status(400).json(`Valores Obrigatórios não recebidos`)
-
-    if (senha.length < 6)
-        return res.status(400).json(`A Senha deve ter pelo menos 6 caracteres`)
-
-    if (senha != confirmacao)
-        return res.status(400).json(`As senhas não batem`)
 
     let hashedPassword = await bcrypt.hash(senha, 10)
 
-    const lista_emails = await cliente
-                                    .query(`SELECT * FROM users WHERE email = $1`, [email])
+    const lista_usuarios = await cliente
+                                    .query(`SELECT * FROM users WHERE usuario = $1`, [usuario])
                                     .catch(e => {                                  
                                         return res.status(400).json(e)
                                     })
 
-    if (lista_emails.rows.length > 0) {
-        return res.status(400).json(`Esse Email já Foi cadastrado`)
+    if (lista_usuarios.rows.length > 0) {
+        return res.status(400).json(`Esse nome de Usuário Já Existe`)
     }
 
     const cadastro = await cliente
-                                .query(`INSERT INTO users (nome, email, senha) VALUES ($1, $2, $3) RETURNING id, senha`, [nome, email, hashedPassword])
+                                .query(`INSERT INTO users (usuario, senha) VALUES ($1, $2) RETURNING id, senha`, [usuario, hashedPassword])
                                 .catch(e => {                                  
                                     return res.status(400).json(e)
                                 })
 
-    console.log(cadastro.rows)
     return res.status(200).json(`Usuário Cadastrado, Por Favor, faça Login`)
 })
 
 module.exports = user
-
-/*
-CREATE TABLE users (
-	id BIGSERIAL PRIMARY KEY NOT NULL,
-	nome varchar(200) not null,
-	email varchar(200) not null,
-	senha varchar(200) not null,
-	unique(email)
-);
-
-select * from users
-
-alter table users
-add column senha varchar(200) not null
-
-INSERT INTO users (nome, email, senha) VALUES ('admin', 'admin@gmail.com', '123456')
-delete from users*/
