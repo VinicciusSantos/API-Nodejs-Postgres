7f4efc66483cd3d8eb8ed7d7ae423e37d9b759db
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2022-08-20 17:35:10 -0300

    Middleware de Autenticação

--- cmd/jwt.js
+++ cmd/jwt.js
@@ -1,2 +1,14 @@
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+    const token = req.headers['x-access-token']
+  
+    jwt.verify(token, process.env.SECRET, (err, decored) => {
+        if (err) return res.status(401).json({message: "Não Autorizado"}).end()
+
+        req.id = decored.id;
+        next();
+    })
+}
+
+module.exports = authenticateToken
