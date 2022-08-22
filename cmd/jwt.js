94b8dfd88303d84935e2d2aa13e0c8d5d6b90715
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2022-08-22 06:51:49 -0300

    Ajustes finais para autenticação

--- cmd/jwt.js
+++ cmd/jwt.js
@@ -1,7 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
-    const token = req.headers['x-access-token']
+    const header = req.headers.authorization;
+    if (!header){
+        return res.status(401).json({message: `Token Inválido!`});
+    }
+    const token = header.split(' ')[1]
+    console.log(req.headers)
   
     jwt.verify(token, process.env.SECRET, (err, decored) => {
         if (err) return res.status(401).json({message: "Não Autorizado"}).end()
