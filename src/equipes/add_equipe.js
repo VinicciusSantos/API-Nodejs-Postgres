50c51889fc90fdc660c0160db3e7d5994a497b8f
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-08-23 11:22:32 -0300

    Update add_equipe.js
    Fotos para equipes

--- src/equipes/add_equipe.js
+++ src/equipes/add_equipe.js
@@ -2,23 +2,26 @@ const express = require('express')
 const equipes = express.Router()
 var cliente = require('../../cmd/database/connection.js')
 const authenticateToken = require('../../cmd/jwt')
+const multer = require('multer');
+const multerConfig = require('../../cmd/multer');
+const { s3Uploadv2 } = require("../../cmd/s3Service.js");
 
 //Inserindo equipes
-equipes.post('/equipes', authenticateToken, async (req, res) => { 
+equipes.post('/equipes', authenticateToken, multer(multerConfig).single('foto'), async (req, res) => { 
     const body = req.body
 
     if (body.eq_nome === "" || !body.eq_nome) {
         return res.status(400).json('Nome Inválido')
     }
 
-    // cadastrando a equipe
-    cliente.query('INSERT INTO equipes (eq_nome) values ($1)', [body.eq_nome])
+    
+    if (req.file) {
+        var result = await s3Uploadv2(req.file)
+        console.log(result)
+    }   
 
-    const pessoas = await cliente
-                                .query(`select pe_nome, pe_id from pessoas`)
-                                .catch(e => {                                  
-                                    return res.status(400).json(e)
-                                })
+    // cadastrando a equipe
+    cliente.query('INSERT INTO equipes (eq_nome, eq_foto) values ($1, $2)', [body.eq_nome, result.Location])
 
     // Pegando o id da equipe que acabou de ser cadastrada
     const id = await cliente
@@ -29,17 +32,19 @@ equipes.post('/equipes', authenticateToken, async (req, res) => {
 
 
     // Colocando as pessoas na equipe
-    body.pessoas.forEach(async p => {
-        const idPessoa = await cliente.query(`select pe_id from pessoas where pe_nome = $1`, [p])
-        cliente
-            .query(`INSERT INTO pessoas_pertencem_equipes (fk_pessoa, fk_equipe)
-                    VALUES ($1, $2)`, [idPessoa.rows[0].pe_id, id.rows[0].max])
-            .catch(e => {                
-                return res.status(400).json(e)
-            })
-    });
+    if (body.pessoas) {
+        body.pessoas.forEach(async p => {
+            const idPessoa = await cliente.query(`select pe_id from pessoas where pe_nome = $1`, [p])
+            cliente
+                .query(`INSERT INTO pessoas_pertencem_equipes (fk_pessoa, fk_equipe)
+                        VALUES ($1, $2)`, [idPessoa.rows[0].pe_id, id.rows[0].max])
+                .catch(e => {                
+                    return res.status(400).json(e)
+                })
+        });
+    }
 
-    return res.json("Inserido com sucesso!")
+    return res.json(result)
 })
 
 module.exports = equipes
