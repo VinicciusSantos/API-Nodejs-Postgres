db6cc70f91a8f2b4dfa67604dac717208f462827
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-08-31 11:49:44 -0300

    Post de equipe Funcional

--- domain/equipe/usecase.js
+++ domain/equipe/usecase.js
@@ -3,9 +3,5 @@ const Equipe = require('../../infra/equipe/sequelize/data')
 exports.NovaEquipe = async (req, res, NovaEquipe) => {
     NovaEquipe.nome = `Equipe: ${NovaEquipe.nome}`
 
-    try {
-        const resp = await Equipe.NovaEquipe(NovaEquipe)
-        console.log(resp)
-        return res.status(201).json({message: "Criado com sucesso", response: resp})
-    } catch (err) { return res.status(400).json({message: "Erro", err}) }
+    return await Equipe.NovaEquipe(NovaEquipe)
 }
--- webservice/equipe/handler.js
+++ webservice/equipe/handler.js
@@ -1,9 +1,14 @@
 const Equipe = require('../../domain/equipe/usecase')
 const ModelApresentacao = require('../../domain/equipe/model/model')
 
-exports.NovaEquipe = (req, res) => {
+exports.NovaEquipe = async (req, res) => {
     const { nome, foto } = req.body
     let equipeNova = new ModelApresentacao(nome, foto)
 
-    Equipe.NovaEquipe(req, res, equipeNova)
+    try {
+        const novosDados = await Equipe.NovaEquipe(req, res, equipeNova)
+        return res.status(201).json({message: "Criado com Sucesso", data: novosDados})
+    } catch (error) {
+        return res.status(400).json({message: "Não Foi possível criar a equipe", error: error})
+    }
 }
