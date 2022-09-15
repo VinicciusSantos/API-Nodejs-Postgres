db1d1d3a2a0ff231e44dbd6da5e6886e3db57218
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2022-09-15 18:47:37 -0300

    limite das fotos no multer

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -4,9 +4,6 @@ const storage = multer.memoryStorage()
 module.exports = {
   dest: "./uploads",
   storage: storage,
-  limits: {
-    fileSize: 2 * 1024 * 1024,
-  },
   fileFilter: (req, file, cb) => {
     const allowedMimes = [
       "image/jpeg",
